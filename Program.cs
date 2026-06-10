b17963455610ec0fc8a15061920a7e6792723a1c
stian126jg-svg <stian1.26jg@gmail.com>
2026-06-10 09:49:41 +0200

    Established User Profile Class and Greeting Lookup Table.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
 ﻿// Date/Time Console / Simple Tasklist/Appointment booker
 
+
+// User Profile Class
+public class UserProfile
+{
+    public string Name { get; set; } = "";
+    public string Gender { get; set; } = "";
+    public DateTime DateOfBirth { get; set; }
+}
+
+// Greeting Lookup Table
+Dictionary<string, string> greetings = new()
+{
+    { "Morning", "Good Morning" },
+    { "Afternoon", "Good Afternoon"},
+    { "Evening", "Good Evening" }
+};
+
+string GetTimePeriod ()
+{
+    int hour = DateTime.Now.Hour;
+
+    if (hour < 12)
+        return "Morning";
+
+    if (hour < 18)
+        return "Afternoon";
+
+    return "Evening";
+}
+
+string period = GetTimePeriod();
+
+Console.WriteLine($"{greetings[period]}, {user.Name}!");
