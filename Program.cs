ea04d6a9287d47b9456b757f90f606a7421aef95
stian126jg-svg <stian1.26jg@gmail.com>
2026-06-10 10:58:47 +0200

    Implemented Main Dashboard Loop. Not complete, but most of it is skeleton for what is to come

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Text.Json;
 
 UserProfile user;
+
 
 // Load existing profile or create one
 if (File.Exists("user.json"))
@@ -14,3 +16,75 @@ else
     user = CreateProfile();
     SaveProfile(user);
 }
+
+
+// Main Dashboard Loop
+while (true)
+{
+    DisplayDashboard(user);
+
+    Thread.Sleep(1000);
+}
+
+static UserProfile CreateProfile()
+{
+    Console.Write("Enter your name: ");
+    string name = Console.ReadLine() ?? "";
+
+    Console.Write("Enter your gender: ");
+    string gender = Console.ReadLine() ?? "";
+
+    Console.Write("Enter your date of birth: (yyyy-MM-dd): ");
+    DateTime dob = DateTime.Parse(Console.ReadLine() ?? "");
+
+    return new UserProfile
+    {
+        Name = name,
+        Gender = gender,
+        DateOfBirth = dob
+    };
+}
+
+
+static void SaveProfile(UserProfile user)
+{
+    string json = JsonSerializer.Serialize(
+        user,
+        new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+    
+    File.WriteAllText("user.json", json);
+}
+
+static bool IsBirthday(DateTime birthday)
+{
+    DateTime today = DateTime.Today;
+
+    return birthday.Month == today.Month &&
+           birthday.Day == today.Day;
+}
+
+static string GetGreeting()
+{
+    Directory<string, string> greetings = new()
+    {
+        { "Morning", "Good Morning" },
+        { "Afternoon", "Good Afternoon" },
+        { "Evening", "Good Evening" }
+    };
+
+    int hour = DateTime.Now.Hour;
+
+    string period = hour switch
+    {
+        < 12 => "Morning",
+        < 18 => "Afternoon",
+        _ => "Evening"
+    };
+
+    return greetings[period];
+}
+
+
